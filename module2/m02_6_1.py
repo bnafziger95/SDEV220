657b682977debc45eb5e447543d9f3f86cc48cb8
bnafziger95 <bnafziger95@gmail.com>
2026-02-08 21:30:18 -0500

    Add files via upload

---
+++ module2/m02_6_1.py
@@ -0,0 +1,13 @@
+# 6.1 - if/else/elif practice
+
+# variables
+secret = 7
+guess = 3
+
+# conditional tests
+if guess < secret:
+    print("too low")
+if guess > secret:
+    print("too high")
+elif guess == secret:
+    print("just right")
